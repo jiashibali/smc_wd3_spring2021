cec6ccc34781a8cf5523ccf36c858b86d9cbe44d
jiashibali <jiashibali@outlook.com>
2021-04-25 22:38:23 -0700

    week 9 homework

--- week09/homework/script.js
+++ week09/homework/script.js
@@ -1,3 +1,14 @@
 $('.f1_container').click(function() {
     $(this).toggleClass('active');
 });
+
+
+function changeImg() {
+    var image = document.getElementById('logo');
+    if (image.src.match("img/kdawhite.png")) {
+        image.src = "img/kdaglow.png";
+    }
+    else {
+        image.src = "img/kdawhite.png";
+    }
+}
